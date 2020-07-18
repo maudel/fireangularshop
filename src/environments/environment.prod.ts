2985f9180e915a69494d47469e2529f0f203fabc
Maudel <maudelaquintana@gmail.com>
2020-07-18 23:56:41 +0200

    fire hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCsvb3lewjZ0fRw9nFZHNGuambzQFwth9s',
+    authDomain: 'fir-angularstdjam.firebaseapp.com',
+    databaseURL: 'https://fir-angularstdjam.firebaseio.com',
+    projectId: 'fir-angularstdjam',
+    storageBucket: 'fir-angularstdjam.appspot.com',
+    messagingSenderId: '854106189894',
+    appId: '1:854106189894:web:1c49d7197da1a5a9941a47',
+    measurementId: 'G-4F8H9K878K'
+  }
 };
